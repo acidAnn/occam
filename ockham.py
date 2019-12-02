eee1513037c6403b44b68b69b2c42e38dbd80c48
acid_ann <auersanna@googlemail.com>
2019-12-02 19:48:45 +0100

    Issue #4: Fix newline conundrum

--- ockham.py
+++ ockham.py
@@ -4,27 +4,30 @@ import os
 
 nlp = spacy.load("de_core_news_sm")
 
+
 @click.command()
-@click.option('--file', help='the file with the text to be processed')
+@click.option("--file", help="the file with the text to be processed")
 def ockham(file):
     """Remove adjectives from a text to make it more stylistically concise."""
-#    print(os.path.abspath(file))
     with open(file) as f:
         text = f.read()
 
     if text:
         doc = nlp(text)
-    
-        result_text = ""
-        for token in doc:
-            if token.pos_ not in ["ADJ", "ADV"]:
-                if token.pos_ != "PUNCT":
-                   result_text += " "
 
+        result_text = ""
+        for i, token in enumerate(doc):
+            if token.pos_ not in ["ADJ", "ADV"]:
                 result_text += token.text
-     
+                if (
+                    i < len(doc) - 1
+                    and token.pos_ != "SPACE"
+                    and token.nbor().pos_ not in ["PUNCT", "SPACE", "X"]
+                ):
+                    result_text += " "
+
         print(result_text.strip())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     ockham()
