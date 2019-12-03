f49178f388abc5dfc48d4f02abe399fce55537b9
acid_ann <auersanna@googlemail.com>
2019-12-03 19:54:19 +0100

    Issue #1 Combine options --str and --file

--- occam.py
+++ occam.py
@@ -6,27 +6,36 @@ nlp = spacy.load("de_core_news_sm")
 
 
 @click.command()
-@click.option("--file", help="the file with the text to be processed")
-def ockham(file):
+@click.option("--file", "-f", "filepath_", default="", help="the file with the text to be processed")
+@click.option("--str", "-s", "str_", default="", help="the string with the text to be processed")
+def ockham(filepath_, str_):
     """Remove adjectives from a text to make it more stylistically concise."""
-    with open(file) as f:
-        text = f.read()
+    text = ""
 
-    if text:
-        doc = nlp(text)
+    if filepath_:
+        with open(filepath_) as f:
+            text = f.read()
 
-        result_text = ""
-        for i, token in enumerate(doc):
-            if token.pos_ not in ["ADJ", "ADV"]:
-                result_text += token.text
-                if (
-                    i < len(doc) - 1
-                    and token.pos_ != "SPACE"
-                    and token.nbor().pos_ not in ["PUNCT", "SPACE", "X"]
-                ):
-                    result_text += " "
+    if str_:
+        text += str_
 
-        print(result_text.strip())
+    if not text:
+        raise click.BadParameter("you must supply the option --file or --str for input")
+
+    doc = nlp(text)
+
+    result_text = ""
+    for i, token in enumerate(doc):
+        if token.pos_ not in ["ADJ", "ADV"]:
+            result_text += token.text
+            if (
+                i < len(doc) - 1
+                and token.pos_ != "SPACE"
+                and token.nbor().pos_ not in ["PUNCT", "SPACE", "X"]
+            ):
+                result_text += " "
+
+    print(result_text.strip())
 
 
 if __name__ == "__main__":
