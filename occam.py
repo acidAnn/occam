762d160ac6a439bb7fe5bb75864c534cf5aafc00
acid_ann <auersanna@googlemail.com>
2019-12-05 22:10:12 +0100

    Less :spaghetti:

--- occam.py
+++ occam.py
@@ -3,48 +3,93 @@
 Functions:
 occam
 """
+from typing import List, Tuple
+
 import spacy
 import click
 import math
-import os
 
 nlp = spacy.load("de_core_news_sm")
 
 
 @click.command()
-@click.option("--file", "-f", "filepath_", default="", help="File with text input")
-@click.option("--text", "-t", default="", help="String with text input")
-@click.option("--output", "-o", default="", help="Path to output file (optional, otherwise print to CLI)")
-@click.option("--count", "-c", is_flag=True, help="Display the number of removed words")
-@click.option("--sub", "-s", default="", help="String by which adjectives and adverbs shall be replaced")
-@click.option("--adj", "-j", is_flag=True, help="Only remove adjectives")
-@click.option("--adv", "-v", is_flag=True, help="Only remove adverbs")
-def occam(filepath_, text, count, sub, adj, adv, output):
+@click.option("--file", "-f", "filename_", default="", help="File with text input.")
+@click.option("--text", "-t", default="", help="String with text input.")
+@click.option("--output", "-o", default="", help="Path to output file (optional, otherwise print to CLI).")
+@click.option("--adj", "-j", is_flag=True, help="Only remove adjectives (default: remove adjectives and adverbs).")
+@click.option("--adv", "-v", is_flag=True, help="Only remove adverbs.")
+@click.option("--sub", "-s", default="", help="String by which adjectives and adverbs shall be replaced.")
+@click.option("--count", "-c", is_flag=True, help="Display the number of removed words.")
+def occam(filename_: str, text: str, output: str, adj: bool, adv: bool, sub: str, count: bool):
     """Remove adjectives and adverbs from text."""
+    input_text = determine_input_text(filename_, text)
+    text_length = len(input_text)
+    doc = nlp(input_text)
+
+    removables = determine_removables(adj, adv)
+
+    output_text, counter = remove_removables(doc, text_length, removables, sub)
+
+    if count:
+        print_counter(counter, text_length)
+
+    output_text = output_text.strip()
+    show_output_text(output, output_text)
+
+
+def determine_input_text(filepath_: str, text: str) -> str:
+    """Determine whether input was supplied directly in a string or indirectly in a file.
+
+    :param filepath_: str representing the path to an text file (empty if no file supplied by user)
+    :param text: str representing the text input (empty if no string supplied by user)
+    :return: the input text as a str
+    """
     input_text = ""
-    output_text = ""
-    counter = 0
 
     if text:
         input_text += text
 
+    # if both a string and a file path are supplied, they are concatenated to a single input text
     if filepath_:
         with open(filepath_) as f:
             input_text += f.read()
 
     if not input_text:
-        raise click.BadParameter("You must supply the option --file or --text for text input.")
+        raise click.BadParameter(
+            "You must supply the option --file or --text for text input."
+        )
 
-    doc = nlp(input_text)
+    return input_text
 
+
+def determine_removables(adj: bool, adv: bool) -> List[str]:
+    """Decide if only adjectives, only adverbs or both shall be removed.
+
+    :param adj: bool representing whether adjectives shall be removed
+    :param adv: bool representing whether adverbs shall be removed
+    :return: a list of part of speech tags representing the POS to be removed
+    """
     if adj:
-        removables = ["ADJ"]
+        return ["ADJ"]
 
     elif adv:
-        removables = ["ADV"]
-    
+        return ["ADV"]
+
     else:
-        removables = ["ADJ", "ADV"]
+        return ["ADJ", "ADV"]
+
+
+def remove_removables(doc, text_length: int, removables: List[str], sub: str) -> Tuple[str, int]:
+    """Remove adjectives and/or adverbs from doc.
+
+    :param doc: the input text as a spacy Doc object
+    :param text_length: the input length as an int
+    :param removables: a List of part of speech tags representing the POS to be removed
+    :param sub: str by which the removables shall be replaced (empty if not supplied by user)
+    :return: the output text without removables, the number of removed tokens
+    """
+    output_text = ""
+    counter = 0
 
     for i, token in enumerate(doc):
         if token.pos_ in removables:
@@ -53,40 +98,64 @@ def occam(filepath_, text, count, sub, adj, adv, output):
             if sub:
                 output_text += sub
 
-                if (
-                    i < len(doc) - 1
-                    and token.pos_ != "SPACE"
-                    and token.nbor().pos_ not in ["PUNCT", "SPACE", "X"]
-                ):
-                    output_text += " "
-
+            if whitespace_needed(i, text_length, token):
+                output_text += " "
 
         else:
             output_text += token.text
 
-            if (
-                i < len(doc) - 1
-                and token.pos_ != "SPACE"
-                and token.nbor().pos_ not in ["PUNCT", "SPACE", "X"]
-            ):
+            if whitespace_needed(i, text_length, token):
                 output_text += " "
 
-    if count:
-        doc_length = len(input_text)
-        if doc_length > 0 and counter > 0:
-            print("-- {} words out of {} ({}%) have been removed --".format(counter, doc_length, math.ceil(counter/doc_length*100)))
+    return output_text, counter
 
-        else:
-            print("-- nothing to remove: Occam would be proud of you :) --")
-        
-    output_text = output_text.strip()
 
-    if output:
-       with open(output, mode='w') as f:
-           f.write(output_text)
-       
+def whitespace_needed(index: int, text_length: int, token) -> bool:
+    """Determine if a token and its predecessor should be seperated by whitespace.
+
+    :param index: the int index of token in the input text
+    :param text_length: the length of the input text
+    :param token: the spacy Token object to be considered
+    :return: True if whitespace is needed
+             False otherwise
+    """
+    return (
+        index < text_length - 1
+        and token.pos_ != "SPACE"
+        and token.nbor().pos_ not in ["PUNCT", "SPACE", "X"]
+    )
+
+
+def print_counter(counter: int, text_length: int):
+    """Print the number of removed words relative to the text length.
+   
+   :param counter: int representing the number of removed words
+   :param text_length: int representing the overall text length
+   """
+    if text_length > 0 and counter > 0:
+        removed_percentage = math.ceil(counter / text_length * 100)
+        print(
+            "-- {} words out of {} ({}%) have been removed --".format(
+                counter, text_length, removed_percentage
+            )
+        )
+
     else:
-       print(output_text.strip())
+        print("-- nothing to remove: Occam would be proud of you :) --")
+
+
+def show_output_text(output: str, output_text: str):
+    """Provide the output text to the user, either by printing it or writing it to a file.
+
+    :param output: the path to an output file (empty if not supplied by the user)
+    :param output_text: the result str
+    """
+    if output:
+        with open(output, mode="w") as f:
+            f.write(output_text)
+
+    else:
+        print(output_text)
 
 
 if __name__ == "__main__":
